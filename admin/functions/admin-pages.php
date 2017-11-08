7c09a82a0db56b9c6aa7c4570883debcdf442925
Mikhail Valentsev <michael@valentsev.ru>
2017-11-08 13:29:19 +0500

    don't understand what i'm doing markup

--- admin/functions/admin-pages.php
+++ admin/functions/admin-pages.php
@@ -15,6 +15,260 @@ function ibrw_solutions() {
 
 function ibrw_types() {
 	?>
-<h1>Here should be the solution</h1>
+
+<div class="b-parameters">
+	<div class="b-parameters-fieldset">
+		<div class="b-parameters-fieldset-title">Security</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Subscription Recovery</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item type_1">
+					<input type="radio" name="subscription_recovery">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_2">
+					<input type="radio" name="subscription_recovery">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_3">
+					<input type="radio" name="subscription_recovery">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="subscription_recovery" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="subscription_recovery">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Captcha</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item type_1">
+					<input type="radio" name="captcha" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_2">
+					<input type="radio" name="captcha">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_3">
+					<input type="radio" name="captcha">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="captcha">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="captcha">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Spam Protection</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item type_1">
+					<input type="radio" name="spam_protection" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_2">
+					<input type="radio" name="spam_protection">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_3">
+					<input type="radio" name="spam_protection">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="spam_protection">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="spam_protection">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+	</div>
+	<div class="b-parameters-fieldset">
+		<div class="b-parameters-fieldset-title">Templates</div>
+		<div class="b-parameters-fieldset-field">
+        	<div class="b-parameters-fieldset-field-title">Pop-up</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item for_text">
+					<input type="text" value="8">
+				</label>
+				<label class="b-parameters-fieldset-field-item type_6">
+					<input type="radio" name="pop-up">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="pop-up">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="pop-up">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Inline</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item for_text">
+					<input type="text" value="?">
+				</label>
+				<label class="b-parameters-fieldset-field-item type_6">
+					<input type="radio" name="inline">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="inline">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="inline" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+	</div>
+	<div class="b-parameters-fieldset">
+		<div class="b-parameters-fieldset-title">Targeting</div>
+		<div class="b-parameters-fieldset-subtitle">Live Triggers</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Leave Intention</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item type_1">
+					<input type="radio" name="leave_intention" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_2">
+					<input type="radio" name="leave_intention">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_3">
+					<input type="radio" name="leave_intention">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="leave_intention">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="leave_intention">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Time on Page</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item type_1">
+					<input type="radio" name="time_on_page" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_2">
+					<input type="radio" name="time_on_page">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_3">
+					<input type="radio" name="time_on_page">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="time_on_page">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="time_on_page">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+	</div>
+	<div class="b-parameters-fieldset">
+		<div class="b-parameters-fieldset-title">Plan</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Price</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item for_text">
+					<input type="text" value="384">
+				</label>
+				<label class="b-parameters-fieldset-field-item for_select">
+					<select>
+						<option>/ year</option>
+						<option>/ month</option>
+					</select>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="days_for_refund">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Free Trial</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item type_1">
+					<input type="radio" name="free_trial">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_2">
+					<input type="radio" name="free_trial">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_3">
+					<input type="radio" name="free_trial">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="free_trial" checked>
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="free_trial">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+		<div class="b-parameters-fieldset-field">
+			<div class="b-parameters-fieldset-field-title">Days for Refund</div>
+			<div class="b-parameters-fieldset-field-group">
+				<label class="b-parameters-fieldset-field-item for_text">
+					<input type="text" value="14">
+				</label>
+				<label class="b-parameters-fieldset-field-item type_6">
+					<input type="radio" name="days_for_refund">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_4">
+					<input type="radio" name="days_for_refund">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+				<label class="b-parameters-fieldset-field-item type_5">
+					<input type="radio" name="days_for_refund">
+					<span class="b-parameters-fieldset-field-icon"></span>
+				</label>
+			</div>
+			<input class="b-parameters-fieldset-field-comment" type="text" placeholder="Comment">
+		</div>
+	</div>
+</div>
+
 <?php
 }
